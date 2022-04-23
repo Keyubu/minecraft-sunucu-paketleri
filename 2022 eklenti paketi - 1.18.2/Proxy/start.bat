b135ad66d429e68530d617dfe37e38eeb85548df
Over_Brave <over55brave@gmail.com>
2022-04-23 12:00:17 +0300

    Proxy eklendi.

---
+++ 2022 eklenti paketi - 1.18.2/Proxy/start.bat
@@ -0,0 +1,19 @@
+﻿::  _                         _                                       
+:: | |                       | |                                      
+:: | | __  ___  _   _  _   _ | |__   _   _   
+:: | |/ / / _ \| | | || | | || '_ \ | | | |
+:: |   < |  __/| |_| || |_| || |_) || |_| |
+:: |_|\_\ \___| \__, | \__,_||_.__/  \__,_|
+::               __/ |                                                
+::              |___/     
+:: 
+@echo off
+title Keyubu Velocity Proxy
+:start
+echo Keyubu Velocity Proxy paketi etkinlestiriliyor...
+java -Xms256M -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -Dfile.encoding=UTF-8 -Djline.terminal=jline.UnsupportedTerminal -jar Proxy.jar
+:: Buradan asagisini, sunucunuzun kapandiginda tekrar baslatılmasını istemiyorsanız silebilirsiniz.
+echo Sunucu kapatildi. Yeniden aktif edilmesini istemiyorsaniz pencereyi kapatin...
+echo (10 saniye sonra aktif edilecek...)
+timeout 10 > null
+goto start
--- 2022 eklenti paketi - 1.18.2/Skyblock/start.bat
+++ 2022 eklenti paketi - 1.18.2/Skyblock/start.bat
@@ -12,7 +12,7 @@ title Keyubu 1.18.2 Skyblock
 :start
 echo Keyubu 1.18.2 Skyblock paketi etkinlestiriliyor...
 java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
-:: Buradan asagisini, sunucunuzun kapandiginde tekrar baslatılmasını istemiyorsanız silebilirsiniz.
+:: Buradan asagisini, sunucunuzun kapandiginda tekrar baslatılmasını istemiyorsanız silebilirsiniz.
 echo Sunucu kapatildi. Yeniden aktif edilmesini istemiyorsaniz pencereyi kapatin...
 echo (10 saniye sonra aktif edilecek...)
 timeout 10 > null
